f543df824d1ecaf8e3cb58ca32ac43c5cf9ba27e
Oleg Sementsov <tqdr00@gmail.com>
2022-07-07 22:39:21 +0300

    added simple decorator example using agregation (#11)
    Co-authored-by: EPAM\Oleg_Sementsov <oleg_sementsov@epam.com>

---
+++ Patterns/StructuralPatterns/Decorator/DecoratorNotifier/Notifiers/NotifierBase.cs
@@ -0,0 +1,7 @@
+﻿namespace StructuralPatterns.Decorator.DecoratorNotifier.Notifiers
+{
+    internal abstract class NotifierBase
+    {
+        public abstract void Notify();
+    }
+}
---
+++ Patterns/StructuralPatterns/Decorator/DecoratorNotifier/Notifiers/NotifiersDecorator.cs
@@ -0,0 +1,25 @@
+﻿namespace StructuralPatterns.Decorator.DecoratorNotifier.Notifiers
+{
+    internal class NotifiersDecorator : NotifierBase
+    {
+        protected List<NotifierBase> Notifiers;
+        public NotifiersDecorator()
+        {
+                Notifiers = new List<NotifierBase>();
+        }
+
+        public NotifiersDecorator AddToNotifiers(NotifierBase notifier)
+        {
+            Notifiers.Add(notifier);
+            return this;
+        }
+
+        public override void Notify()
+        {
+            foreach (var item in Notifiers)
+            {
+                item.Notify();
+            }
+        }
+    }
+}
